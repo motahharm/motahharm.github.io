d6970093ef9e7a7928e5e27ec75447380b1587d5
Motahhar Mokfi <motahharmokfi@gmail.com>
2022-09-06 15:38:18 +0430

    add metatags

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // STYLE
 import './index.css';
-// import 'https://cdnjs.cloudflare.com/ajax/libs/vazir-font/30.1.0/font-face.css'; // Font
 // PAGES
 import Home from './pages/Home';
 
